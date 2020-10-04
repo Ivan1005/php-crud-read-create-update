6f0e3a7150cb9d9355391f123f1940dbff5ab3ea
Ivan <matteraivam@gmail.com>
2020-10-04 11:37:03 +0200

    aaa

--- partials-2/env.php
+++ partials-2/env.php
@@ -3,4 +3,8 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "dbhotel";
+$host = "http://localhost/";
+$folder = "29/php-crud-read-create-update";
+$basepath = $host.$folder;
+
 ?>
